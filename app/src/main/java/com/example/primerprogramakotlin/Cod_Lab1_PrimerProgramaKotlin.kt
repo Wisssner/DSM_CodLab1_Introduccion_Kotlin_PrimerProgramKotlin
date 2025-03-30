a79ed46f153521c9f47ce0b6835bc7f0302fa945
wisner <wisner.valdiviezo@unmsm.edu.pe>
2025-03-30 16:27:59 -0500

    Initial commit

---
+++ app/src/main/java/com/example/primerprogramakotlin/Cod_Lab1_PrimerProgramaKotlin.kt
@@ -0,0 +1,39 @@
+package com.example.primerprogramakotlin
+
+fun main(){
+
+    ejercicios()
+}
+
+fun ejercicios() {
+    // Ejercicio 1: Imprime los números en orden
+    println("1")
+    println("2")
+    println("3")
+
+    // Ejercicio 2: Mensajes sobre Kotlin
+    println("I'm")
+    println("learning")
+    println("Kotlin!")
+
+    // Ejercicio 3: Ordenar los días de la semana
+    println("Monday")
+    println("Tuesday")
+    println("Wednesday")
+    println("Thursday")
+    println("Friday")
+
+    // Ejercicio 4: Corregir error en impresión
+    println("Tomorrow is rainy")
+
+    // Ejercicio 5: Corregir error en función de impresión
+    println("There is a chance of snow")
+
+    // Ejercicio 6: Corrección de sintaxis en múltiples println
+    println("Cloudy")
+    println("Partly Cloudy")
+    println("Windy")
+
+    // Ejercicio 7: Corrección de paréntesis incorrectos
+    println("How's the weather today?")
+}
